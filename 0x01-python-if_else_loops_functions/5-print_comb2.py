ea4cc2c320e4bd463e558a3949b35878b37ff7a2
clintaphiwe <clintaphiwe8@gmail.com>
2024-05-04 09:38:34 +0200

    comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,5 @@
 for i in range(100):
     if i < 10:
         print("0" + "{}".format(str(i)), end=", ")
-    elif i < 99:
+    elif i != 99:
         print("{}".format(str(i)), end=", ")
-    else:
-        print("{}".format(str(i)))
