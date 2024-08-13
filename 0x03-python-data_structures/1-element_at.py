a86b22c7b9097040a44bfb303dcc3e430a8aa07b
clintaphiwe <clintaphiwe8@gmail.com>
2024-08-13 15:39:02 +0200

    element at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-def element_at(my_list,idx):
-    # Check whether the index(idx) is lesser than 0 or not negative
+def element_at(my_list, idx):
+    # Check if the index is negative
     if idx < 0:
         return None
-    # Check whether index(idx) is greater or equal to the length of the list
-    elif idx >= len(my_list):
+    # Check if the index is out of range
+    if idx >= len(my_list):
         return None
-    # If index is valid, return the element at that index
-    return "Element at index {} is {}".format(idx ,my_list[idx])
+    # Return the element at the given index
+    return my_list[idx]
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,3 +11,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     #Return the result tuple
     return (a1 + b1, a2 + b2)
+
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
