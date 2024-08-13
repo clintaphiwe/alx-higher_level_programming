2869e7e88a60130daaa85109ab83279c94c4429b
clintaphiwe <clintaphiwe8@gmail.com>
2024-08-13 15:47:30 +0200

    replace in list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,12 +3,11 @@
 def replace_in_list(my_list, idx, element):
     #Check whether the index(idx) is lesser than 0 and not negative
     if idx < 0:
-        print("{}".format(my_list))
+        return(my_list)
     #Check whether index(idx) is greater or equal to the length of the list
     elif idx >= len(my_list):
-        print("{}".format(my_list))
+        return(my_list)
     #If index is valid, replace the element at that index with the new element
     else:
-        print("{}".format(my_list))
         my_list[idx] = element
-        print("{}".format(my_list))
+        return(my_list)
