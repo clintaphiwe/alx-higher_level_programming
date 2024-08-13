cfa0356ef0ea0ef566295b19dd57a239257fa94f
clintaphiwe <clintaphiwe8@gmail.com>
2024-08-13 15:52:42 +0200

    replace in list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,10 +4,12 @@ def replace_in_list(my_list, idx, element):
     #Check whether the index(idx) is lesser than 0 and not negative
     if idx < 0:
         return(my_list)
+    
     #Check whether index(idx) is greater or equal to the length of the list
-    elif idx >= len(my_list):
+    if idx >= len(my_list):
         return(my_list)
+    
     #If index is valid, replace the element at that index with the new element
-    else:
-        my_list[idx] = element
-        return(my_list)
+    my_list[idx] = element
+    return(my_list)
+
