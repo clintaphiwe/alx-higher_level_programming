7b14bc8898b7c1ad8e63753e6258dd31940e64ba
clintaphiwe <clintaphiwe8@gmail.com>
2024-08-14 14:38:57 +0200

    list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    #Check whether the index(idx) is lesser than 0 and not negative
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    
-    #Check whether index(idx) is greater or equal to the length of the list
-    if idx >= len(my_list):
-        return my_list
-    
-    #If index is valid, replace the element at that index with the new element
     my_list[idx] = element
     return my_list
