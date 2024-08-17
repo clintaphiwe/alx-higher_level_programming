294a1b129ac89541ea589e70c165b99e2e0d1923
clintaphiwe <clintaphiwe8@gmail.com>
2024-08-18 00:09:32 +0200

    smile

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-# for i in range(99):
-#     if i < 10:
-#         print("{}".format("0" + str(i)), end=", ")
-#     elif i <= 100:n  
-#         print(",{}".format(i), end=" ")
-
-
-print(", ".join("0{}".format(i) for i in range(1, 10)), end="")
-# for i in range(10, 100):
-#     print(", {}".format(i), end="")
-if i >= 10 and not >= 100:
-    
+for i in range(100):
+    if i < 99:
+        print("{:02d},".format(i), end="")
+    else:
+        print("{:02d}".format(i))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(1, 10):
+for i in range(10):
     for j in range(i + 1, 10):
-        print("{}{}, ".format(i, j), end="")
-print()
+        if i < 8 or j < 9:
+            print("{:02d}{}".format(i, j), end=", ")
+        else:
+            print("{:02d}{}".format(i, j))
